2051bcc125386f8d7fffb0ebd186ce6354e0ca2c
shorbs <benji.shorb@gmail.com>
2025-06-17 11:43:54 -0700

    Simple interface input structure, logicless

--- src/MyDictionary.java
+++ src/MyDictionary.java
@@ -1,13 +1,13 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class MyDictionary {
     private final Map<String, List<DictionaryEntry>> myDictionary = new HashMap<>();
 
-    private final List<String>{}
-
+    private final List<String> exits = new ArrayList<>(Arrays.asList(
+            "!q",
+            "exit",
+            "quit"
+    ));
 
     MyDictionary() {
         loadDictionary();
@@ -17,15 +17,35 @@ public class MyDictionary {
     }
 
     public void runInterface(){
-        int i = 0;
+        int i = 1;
+
+        System.out.println("! Loading data...");
+        loadDictionary();
+        System.out.println("! Loading completed...");
+        System.out.println();
+        System.out.println("===== DICTIONARY 340 JAVA =====");
+        System.out.println("----- Keywords: " + myDictionary.size());
+        System.out.println("----- Definitions: " + "TO BE IMPLEMENTED");
+        System.out.println();
 
         Scanner scanner = new Scanner(System.in);
         String input;
         while (true) {
+            System.out.print("Search ["+i+"]: ");
             input = scanner.nextLine();
-            if (input.equals("exit")) {
+            if (exits.contains(input.toLowerCase())) {
                 break;
             }
+            System.out.println("|");
+            if(input.equalsIgnoreCase("!help") ||  input.equalsIgnoreCase("")){
+                System.out.println("Instructions to be implemented");
+            }
+            System.out.println("|");
+            i++;
         }
+        System.out.println("\n -----THANK YOU-----");
     }
+
+    //IMPLEMENT GET ARGUMENTS METHOD
+    //IMPLEMENT GET TOTAL DEFINITIONS METHOD
 }
